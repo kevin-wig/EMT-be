fc3106afb0e9f33dc5b49200e5f798f3c03b82da
kevin-w <kevin.wiggins.up@gmail.com>
2022-11-07 15:19:05 +0100

    263: CII Calculator API needs another input

--- src/modules/third-party/api-key.module.ts
+++ src/modules/third-party/api-key.module.ts
@@ -1,32 +1,29 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
+import { Module } from '@nestjs/common';
 
 import { ApiKeyService } from './services/api-key.service';
 import { ApiKeyController } from './controllers/api-key.controller';
 import { UsersModule } from '../users/users.module';
-import { TwoFactorModule } from '../two-factor/two-factor.module';
-import { TokensModule } from '../tokens/tokens.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Vessel } from '../vessels/entities/vessel.entity';
-import { VesselType } from '../vessels/entities/vessel-type.entity';
 import { VesselTrip } from '../vessel-trips/entities/vessel-trip.entity';
 import { ApiKey } from './entities/api-key.entity';
-import { VesselsService } from '../vessels/services/vessels.service';
 import { VesselsModule } from '../vessels/vessels.module';
+import { VesselOnboardingLinks } from '../vessel-onboarding-links/entities/vessel-onboarding-links.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
-        ApiKey,
-        Vessel,
-        VesselTrip
+      ApiKey,
+      Vessel,
+      VesselTrip,
+      VesselOnboardingLinks,
     ]),
     UsersModule,
-    VesselsModule
+    VesselsModule,
   ],
   exports: [ApiKeyService],
   controllers: [ApiKeyController],
   providers: [ApiKeyService],
 })
-export class ApiKeyModule {}
+export class ApiKeyModule {
+}
